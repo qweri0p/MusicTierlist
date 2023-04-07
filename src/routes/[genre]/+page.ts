23f7ae21a7d7051695d5f85b75e1d3bb070b5312
qweri0p <71005650+qweri0p@users.noreply.github.com>
2023-04-07 19:01:36 +0200

    oepsie!

--- src/routes/[genre]/+page.ts
+++ src/routes/[genre]/+page.ts
@@ -1,6 +1,5 @@
 import { error } from '@sveltejs/kit';
 import { tags } from '../../data/parser.js';
-export const prerender = true;
 
 export function load({params}) {
     const genre = params.genre
